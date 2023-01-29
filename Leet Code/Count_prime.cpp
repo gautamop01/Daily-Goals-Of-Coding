6e6f7821bb6923ef33ee6ceac9eefb3acb56faef
Gautam <105223230+gautammahar@users.noreply.github.com>
2023-01-30 00:48:48 +0530

    Sieve of Eratostenes - Count Prime - Problem 204
    Sieve of Eratosthenes - Count Prime - Problem 204

--- Leet Code/Count_prime.cpp
+++ Leet Code/Count_prime.cpp
@@ -1,3 +1,5 @@
+// This Code Showing TLE, Thats's why be use other method called "Sieve of Eratosthenes"
+/* 
 class Solution {
     private:
     bool isPrime(int n){
@@ -20,3 +22,27 @@ public:
        return cnt;
     }
 };
+
+*/
+
+// Sieve of Eratosthenes
+class Solution {
+public:
+    int countPrimes(int n) { 
+        int cnt=0;
+        vector<bool>prime(n+1,true);
+        prime[0]=prime[1]=false;
+        for(int i=2;i<n;i++){
+            if(prime[i]){
+                cnt++;
+                for(int j=2*i;j<n;j=j+i){ // Here this step give all Multiply of n is FALSE (e.g. if in first for loop 3 is prime number is proved then in second loop all 3 table number 3,6,9,12,15,18... is Non Prime Proved ) 
+                    prime[j]=0;
+                }
+            }
+        }
+        return cnt;
+        
+    }
+};
+
+
