1f71fc39f75e06b1676ad3b20273c72457bb48db
Gautam <105223230+gautammahar@users.noreply.github.com>
2023-01-31 03:30:40 +0530

    Valid Parentheses - Problem 20
    Valid Parentheses - Problem 20

---
+++ Leet Code/Valid_Parentheses.cpp
@@ -0,0 +1,38 @@
+class Solution
+{
+public:
+    bool isValid(string s)
+    {
+        stack<int> st;
+
+        for (int i = 0; i < s.length(); i++)
+        {
+            char ch = s[i];
+            if (ch == '(' || ch == '[' || ch == '{')
+                st.push(ch);
+            else if (!st.empty())
+            {
+                char c = st.top();
+                if ((ch == ')' && c == '(') || (ch == ']' && c == '[') || (ch == '}' && c == '{'))
+                {
+                    st.pop();
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        if (st.empty())
+        {
+            return true;
+        }
+        else
+            return false;
+    }
+};
