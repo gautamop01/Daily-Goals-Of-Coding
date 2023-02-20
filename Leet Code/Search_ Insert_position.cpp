fa2fe2be3ed317b4b198ab4a2dcdccceab39c1d4
Gautam <105223230+gautammahar@users.noreply.github.com>
2023-02-20 17:25:43 +0530

    Update Search_ Insert_position.cpp

--- Leet Code/Search_ Insert_position.cpp
+++ Leet Code/Search_ Insert_position.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int searchInsert(vector<int>& nums, int target) {
         int n = nums.size();
-        int left = 0, right = n-1;
+        int left = 0, right = n-1 // Mean if array in 7 size then left point to arr[0] and right point to arr[6]
         while (left <= right) {
             int mid = left + (right - left) / 2;
             if (nums[mid] == target) {
