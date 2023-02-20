e8f8e63de7d557518eefee1dac6f33dd8f61c249
Gautam <105223230+gautammahar@users.noreply.github.com>
2023-02-20 17:24:46 +0530

    Update Search_ Insert_position.cpp

--- Leet Code/Search_ Insert_position.cpp
+++ Leet Code/Search_ Insert_position.cpp
@@ -16,3 +16,22 @@ public:
         return left;
     }
 };
+// Using For Loop 
+// class Solution {
+// public:
+//     int searchInsert(vector<int>& nums, int target) {
+//         int n = nums.size();
+//         int left = 0, right = n-1;
+//         for (int mid = (left + right) / 2; left <= right; mid = (left + right) / 2) {
+//             if (nums[mid] == target) {
+//                 return mid;
+//             } else if (nums[mid] < target) {
+//                 left = mid + 1;
+//             } else {
+//                 right = mid - 1;
+//             }
+//         }
+//         return left;
+//     }
+// };
+
