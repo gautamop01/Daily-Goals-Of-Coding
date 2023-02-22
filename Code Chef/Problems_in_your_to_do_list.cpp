35d7a61767b6f55e0c81b5dc36e498118cb4eaf4
Gautam <105223230+gautammahar@users.noreply.github.com>
2023-02-22 19:48:05 +0530

    Easy Problem

--- Code Chef/Problems_in_your_to_do_list.cpp
+++ Code Chef/Problems_in_your_to_do_list.cpp
@@ -1,1 +1,23 @@
 // Problems in your to-do list
+#include <iostream>
+using namespace std;
+
+int main() { int t;
+cin>>t;
+while(t--){
+    int n;
+    cin>>n;
+    int a[n];
+    for(int i=0;i<n;i++){
+        cin>>a[i];
+    }
+    int cnt=0;
+    for(int i=0;i<n;i++){
+        if(a[i]>=1000) cnt++;
+        else 0;
+    }
+    cout<<cnt<<endl;
+}
+	// your code goes here
+	return 0;
+}
