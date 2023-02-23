59652087368e3ce609e4862c2c66b7c2cb57992d
Gautam <105223230+gautammahar@users.noreply.github.com>
2023-02-23 18:35:44 +0530

    Update Bitwise_Equation.cpp

--- Code Chef/Bitwise_Equation.cpp
+++ Code Chef/Bitwise_Equation.cpp
@@ -1,3 +1,18 @@
+// Problem -
+// Given an integer N, find four positive distinct integers a, b, cand dsuch that:
+// 1sa,b, c, d< 10
+// (a & b) |c) ed=N
+// Here &, , and e represent bitwise AND, OR and xOR, respectively.
+// if there are multiple answers, print any of them. If no answer exists, print -1.
+// Input Format
+// The first line of input will contain a single integer 1, denoting the number of test cases.
+// Each test case consists of one line of input, contalning a single integer N.
+// Output Format
+// For each test case, output -1 if there is no way to find four distinct integers to satisfy the equation.
+// Otherwise, print on a new line any four space-separated integers a, b, cand dthat satisfy the conditions.
+
+
+
 #include <iostream>
 #include <vector>
 
