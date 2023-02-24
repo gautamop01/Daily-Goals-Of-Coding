bd7eff176cacf335bdc48a29bd4364fbdebcebf7
Gautam <105223230+gautammahar@users.noreply.github.com>
2023-02-24 10:52:19 +0530

    Devu and friendship testing
    Devu and friendship testing

---
+++ Code Chef/Practice Problems/Devu_and_friendship_testing.cpp
@@ -0,0 +1,46 @@
+// #include <iostream>
+// using namespace std;
+
+// int main() { int t;
+// cin>>t;
+// while(t--){
+//     int n;
+//     cin>>n;
+//     // int a[n];
+//     // for(int i=0;i<n;i++){
+//     //     cin>>a[i];
+//     // }
+//     // for(int i=0;i<n;i++){
+//     //     cout<<a[n-1]<<endl;
+//     // }
+//     int i,d;
+//     cin>>i>>d;
+//     cout<<d<<endl;
+// }
+// 	// your code goes here
+// 	return 0;
+// }
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int testOp;
+	cin>>testOp;
+	while(testOp--){
+	    
+	    int n;
+	    cin>>n;
+	    int a[n];
+	    for(int i=0;i<n;i++){
+	        cin>>a[i];
+	    }
+	    sort(a,a+n);
+	    int count=0;
+	    for(int i=0;i<n;i++){
+	        if(a[i]!=a[i+1]) count++;
+	    }
+	    cout<<count<<endl;
+	}
+	return 0;
+}
