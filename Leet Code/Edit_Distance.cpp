eb5bac9aea9cfffe214973210e42fbbefe54d0d4
Gautam <105223230+gautammahar@users.noreply.github.com>
2023-02-26 14:54:53 +0530

    Create Edit_Distance.cpp

---
+++ Leet Code/Edit_Distance.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    int minDistance(string word1, string word2) {
+    int m = word1.size();
+    int n = word2.size();
+    
+    // Initialize the dp table with all zeros
+    vector<vector<int>> dp(m+1, vector<int>(n+1, 0));
+    
+    // Fill the first row and first column of the dp table
+    for(int i = 0; i <= m; i++) {
+        dp[i][0] = i;
+    }
+    for(int j = 0; j <= n; j++) {
+        dp[0][j] = j;
+    }
+    
+    // Fill the rest of the dp table
+    for(int i = 1; i <= m; i++) {
+        for(int j = 1; j <= n; j++) {
+            if(word1[i-1] == word2[j-1]) {
+                dp[i][j] = dp[i-1][j-1];
+            } else {
+                dp[i][j] = 1 + min({dp[i-1][j], dp[i][j-1], dp[i-1][j-1]});
+            }
+        }
+    }
+    
+    // Return the result
+    return dp[m][n];
+        
+    }
+};
