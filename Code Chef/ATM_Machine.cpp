20e306c170dfb6f3a092f52b515a81d2efff232d
Gautam <105223230+gautammahar@users.noreply.github.com>
2023-02-27 21:12:26 +0530

    Create ATM_Machine.cpp

---
+++ Code Chef/ATM_Machine.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main() { int t;
+cin>>t;
+while(t--){
+    int n,k;
+    cin>>n>>k;
+    int a[n];
+    for(int i=0;i<n;i++){
+        cin>>a[i];
+    }
+    for(int i=0;i<n;i++){
+      if(a[i]<=k){
+          k=k-a[i];
+          cout<<"1";
+      } else{
+          cout<<"0";
+      }
+    }
+   cout<<endl;
+}
+	// your code goes here
+	return 0;
+}
