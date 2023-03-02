3205d316e674db8c79d80dc2ea4a698e10749037
Gautam <105223230+gautammahar@users.noreply.github.com>
2023-03-03 03:43:50 +0530

    Create Average_permutation.cpp

---
+++ Code Chef/Practice Problems/Average_permutation.cpp
@@ -0,0 +1,17 @@
+/*You are given an integer N.
+Find a permutation P= |h, Pa, ... , Pw] of the integers {1,2,..., N} such that sum of averages of all
+consecutive triplets is minimized, I.e.
+P+P1+Pi2
+=l
+is minimized.
+If multiple permutations are possible, print any of them.
+Input Format
+The first line of Input will contain a single integer T, denoting the number of test cases.
+The first and only line of each test case contains an integer N, the size of the permutation.
+Output Format
+For each test case, output on a new line a permutation which satisfies the above conditions.
+Constraints
+1Ts 1000
+3N 10
+The sum of N over all test cases won't exceed 3. 10°.
+*/
