915c87853b4f82286944efd446fe07e9087233a4
Gautam <105223230+gautammahar@users.noreply.github.com>
2023-03-03 03:44:18 +0530

    Update Average_permutation.cpp

--- Code Chef/Practice Problems/Average_permutation.cpp
+++ Code Chef/Practice Problems/Average_permutation.cpp
@@ -15,3 +15,44 @@ Constraints
 3N 10
 The sum of N over all test cases won't exceed 3. 10°.
 */
+#include <iostream>
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+    while(t--)
+    {
+        int n, x = 1;
+
+        // Taking the input value of N, the size of the permutation.
+        cin >> n;
+
+        // Creating an array of size N to store the permutation.
+        int a[n];
+
+        // Initializing the first four elements of the permutation in the required order.
+        a[0] = n;
+        a[1] = n - 3;
+        a[n - 1] = n - 1;
+        a[n - 2] = n - 2;
+
+        // Filling the remaining elements of the permutation in increasing order.
+        for(int i = 2; i < n; ++i)
+        {
+            // Skipping the last two elements, as they have already been initialized.
+            if((i != n - 1) && (i != n - 2))
+            {
+                a[i] = x;
+                x++;
+            }
+        }
+
+        // Printing the permutation.
+        for(int i = 0; i < n; ++i)
+            cout << a[i] << " ";
+
+        cout << endl;
+    }
+    return 0;
+}
