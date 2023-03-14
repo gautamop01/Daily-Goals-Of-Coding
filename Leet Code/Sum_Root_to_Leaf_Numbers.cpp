b0c4dc19d78dee671c1892d8b20c5751174d230c
Gautam Kumar Mahar <105223230+gautammahar@users.noreply.github.com>
2023-03-14 11:21:24 +0530

    Create Sum_Root_to_Leaf_Numbers.cpp

---
+++ Leet Code/Sum_Root_to_Leaf_Numbers.cpp
@@ -0,0 +1,99 @@
+/*
+You are given the root of a binary tree containing digits from 0 to 9 only.
+You are given the root of a binary tree containing digits from 0 to 9 only.
+
+Each root-to-leaf path in the tree represents a number.
+
+For example, the root-to-leaf path 1 -> 2 -> 3 represents the number 123.
+Return the total sum of all root-to-leaf numbers. Test cases are generated so that the answer will fit in a 32-bit integer.
+
+A leaf node is a node with no children.
+
+ 
+
+Example 1:
+
+
+Input: root = [1,2,3]
+Output: 25
+Explanation:
+The root-to-leaf path 1->2 represents the number 12.
+The root-to-leaf path 1->3 represents the number 13.
+Therefore, sum = 12 + 13 = 25.
+Example 2:
+
+
+Input: root = [4,9,0,5,1]
+Output: 1026
+Explanation:
+The root-to-leaf path 4->9->5 represents the number 495.
+The root-to-leaf path 4->9->1 represents the number 491.
+The root-to-leaf path 4->0 represents the number 40.
+Therefore, sum = 495 + 491 + 40 = 1026.
+ 
+
+Constraints:
+
+The number of nodes in the tree is in the range [1, 1000].
+0 <= Node.val <= 9
+The depth of the tree will not exceed 10.
+Each root-to-leaf path in the tree represents a number.
+
+For example, the root-to-leaf path 1 -> 2 -> 3 represents the number 123.
+Return the total sum of all root-to-leaf numbers. Test cases are generated so that the answer will fit in a 32-bit integer.
+
+A leaf node is a node with no children.
+
+ 
+
+Example 1:
+
+
+Input: root = [1,2,3]
+Output: 25
+Explanation:
+The root-to-leaf path 1->2 represents the number 12.
+The root-to-leaf path 1->3 represents the number 13.
+Therefore, sum = 12 + 13 = 25.
+Example 2:
+
+
+Input: root = [4,9,0,5,1]
+Output: 1026
+Explanation:
+The root-to-leaf path 4->9->5 represents the number 495.
+The root-to-leaf path 4->9->1 represents the number 491.
+The root-to-leaf path 4->0 represents the number 40.
+Therefore, sum = 495 + 491 + 40 = 1026.
+ 
+
+Constraints:
+
+The number of nodes in the tree is in the range [1, 1000].
+0 <= Node.val <= 9
+The depth of the tree will not exceed 10.
+*/
+
+class Solution {
+public:
+    int sumNumbers(TreeNode* root) {
+        int sum = 0; // variable to store the sum of all root-to-leaf numbers
+        sumNumbersHelper(root, 0, sum); // call helper function with initial current number as 0
+        return sum; // return the sum of all root-to-leaf numbers
+    }
+    
+    void sumNumbersHelper(TreeNode* node, int currentNum, int& sum) {
+        if (node == nullptr) { // if node is null, return
+            return;
+        }
+        
+        currentNum = currentNum * 10 + node->val; // update the current number by adding the value of the current node
+        if (node->left == nullptr && node->right == nullptr) { // if the current node is a leaf node, add the current number to the sum variable
+            sum += currentNum;
+            return;
+        }
+        
+        sumNumbersHelper(node->left, currentNum, sum); // recursively call the helper function on the left child of the current node
+        sumNumbersHelper(node->right, currentNum, sum); // recursively call the helper function on the right child of the current node
+    }
+};
