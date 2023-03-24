dd5419fbf92a496f89884334bb83c3c090d1cefa
Gautam Kumar Mahar <105223230+gautammahar@users.noreply.github.com>
2023-03-24 10:24:35 +0530

    Update Reorder Routes to Make All Paths Lead to the City Zero.cpp

--- Leet Code/Reorder Routes to Make All Paths Lead to the City Zero.cpp
+++ Leet Code/Reorder Routes to Make All Paths Lead to the City Zero.cpp
@@ -1,1 +1,25 @@
 
+class Solution {
+public:
+    int minReorder(int n, vector<vector<int>>& connections) {
+        vector<vector<int>> adj(n);
+        vector<bool> visited(n, false);
+        int ans = 0;
+        for (auto& c : connections) {
+            adj[c[0]].push_back(c[1]);
+            adj[c[1]].push_back(-c[0]);
+        }
+        dfs(0, adj, visited, ans);
+        return ans;
+    }
+    
+    void dfs(int u, vector<vector<int>>& adj, vector<bool>& visited, int& ans) {
+        visited[u] = true;
+        for (int v : adj[u]) {
+            if (!visited[abs(v)]) {
+                ans += v > 0;
+                dfs(abs(v), adj, visited, ans);
+            }
+        }
+    }
+};
