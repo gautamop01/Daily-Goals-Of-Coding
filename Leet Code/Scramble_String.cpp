2faa98b1dcbf8b04ef52e445159005e0c5f12b4e
Gautam Kumar Mahar <105223230+gautammahar@users.noreply.github.com>
2023-03-30 17:29:57 +0530

    Update Scramble_String.cpp

--- Leet Code/Scramble_String.cpp
+++ Leet Code/Scramble_String.cpp
@@ -1,5 +1,4 @@
 /*
-
 We can scramble a string s to get a string t using the following algorithm:
 
 If the length of the string is 1, stop.
@@ -8,8 +7,6 @@ Split the string into two non-empty substrings at a random index, i.e., if the s
 Randomly decide to swap the two substrings or to keep them in the same order. i.e., after this step, s may become s = x + y or s = y + x.
 Apply step 1 recursively on each of the two substrings x and y.
 Given two strings s1 and s2 of the same length, return true if s2 is a scrambled string of s1, otherwise, return false.
-
- 
 
 Example 1:
 
@@ -40,6 +37,8 @@ s1.length == s2.length
 1 <= s1.length <= 30
 s1 and s2 consist of lowercase English letters.
 */
+
+
 class Solution {
 public:
     bool isScramble(string s1, string s2) {
@@ -68,3 +67,5 @@ public:
         return dp[0][0][n];
     }
 };
+
+
