294c85bac6480887814389c7860beec3a8638488
Gautam Kumar Mahar <105223230+gautammahar@users.noreply.github.com>
2023-04-05 02:09:01 +0530

    Codeforces Round 863 (Div. 3)
    Codeforces Round 863 (Div. 3)

---
+++ Code Forces/Restore_the_Array.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+
+void printMinMaxSeries(ll n) {
+    ll currTerm, prevTerm;
+    int i = 0;
+    while (i < n - 1) {
+        cin >> currTerm;
+        if(i == 0) cout << currTerm << " ";
+        else cout << min(currTerm, prevTerm) << " ";
+        prevTerm = currTerm;
+        i++;
+        if(i == n - 1) cout << currTerm << endl;
+    }
+}
+
+int main() {
+    ll t, n;
+    cin >> t;
+    while(t--) {
+        cin >> n;
+        printMinMaxSeries(n);
+    }
+    return 0;
+}
