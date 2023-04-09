c22aa38641b341a427990da829cc3986b92e47b6
Gautam Kumar Mahar <105223230+gautammahar@users.noreply.github.com>
2023-04-09 22:51:27 +0530

    Create Ian_Visits_Mary.cpp

---
+++ Code Forces/Ian_Visits_Mary.cpp
@@ -0,0 +1,18 @@
+// Codeforces Round 865 (Div. 2)
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n1, n2;
+        cin >> n1 >> n2;
+        cout << 2 << endl;
+        cout << 1 << " " << n2 - 1 << endl;
+        cout << n1 << " " << n2 << endl;
+    }
+    return 0;
+}
