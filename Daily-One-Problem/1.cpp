57fd656426145686ad4e0f2c595884d139190bc9
Gautam Kumar Mahar <105223230+gautammahar@users.noreply.github.com>
2023-04-19 22:02:51 +0530

    Create 1.cpp

---
+++ Daily-One-Problem/1.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+
+    while (t--) {
+        int n, k;
+        cin >> n >> k;
+
+        int sum = 0;
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= k; j++) {
+                int room_number = (i * 100) + j;
+                sum += room_number;
+            }
+        }
+
+        cout << sum << endl;
+    }
+
+    return 0;
+}
