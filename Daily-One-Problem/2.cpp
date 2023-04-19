39050e79312f14706d8dde4d7b2c5d84495dc581
Gautam Kumar Mahar <105223230+gautammahar@users.noreply.github.com>
2023-04-19 22:03:09 +0530

    Create 2.cpp

---
+++ Daily-One-Problem/2.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+    int n, k;
+    string a, m;
+    cin >> n >> k >> a >> m;
+
+    vector<int> diff(n);
+    for (int i = 0; i < n; i++) {
+        diff[i] = (m[i] - a[i] + 26) % 26;
+    }
+
+    vector<int> count(26);
+    for (int d : diff) {
+        if (d == 0 || d == 2 * k || d == 24) {
+            continue;
+        }
+        count[d]++;
+    }
+
+    int max_count = *max_element(count.begin(), count.end());
+
+    int num_ops = n - max_count;
+    for (int i = 0; i < n; i++) {
+        if (diff[i] == 0 || diff[i] == 2 * k || diff[i] == 24) {
+            continue;
+        }
+        if (count[diff[i]] == max_count) {
+            if (diff[i] == 2) {
+                num_ops += (max_count - count[0] + k - 1) / k;
+            } else {
+                num_ops++;
+            }
+        }
+    }
+
+    cout << num_ops * k << endl;
+
+    return 0;
+}
