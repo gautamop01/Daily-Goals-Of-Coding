51f8030c728cd02eee6b5c29658927590ab7b973
Gautam Kumar Mahar <105223230+gautammahar@users.noreply.github.com>
2023-04-21 10:44:50 +0530

    Update Profitable_Schemes.cpp

--- Leet Code/Profitable_Schemes.cpp
+++ Leet Code/Profitable_Schemes.cpp
@@ -6,13 +6,16 @@ public:
     {
         if(n<0) return 0;
         if(start==profit.size() && minprofit<=0) return 1;
+        
         if(start==profit.size()) return 0;
         if(dp[n][minprofit][start] != -1) return dp[n][minprofit][start];
+        
         long long a = solve(n-group[start], max(0, minprofit-profit[start]),group, profit, start+1);
-
         long long b = solve(n, minprofit,group, profit, start+1);
+        
         return dp[n][minprofit][start] = (a+b)%mod;
     }
+    
     int profitableSchemes(int n, int minProfit, vector<int>& group, vector<int>& profit) {
         memset(dp, -1, sizeof(dp));
         return solve(n, minProfit, group, profit, 0);
