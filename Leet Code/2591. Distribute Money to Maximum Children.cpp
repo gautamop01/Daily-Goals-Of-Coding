5041a80488894a430cbee0b1da351725f9d0a101
Gautam Kumar Mahar <105223230+gautammahar@users.noreply.github.com>
2023-04-21 14:46:13 +0530

    Create 2591. Distribute Money to Maximum Children.cpp

---
+++ Leet Code/2591. Distribute Money to Maximum Children.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int distMoney(int m, int c) {
+        int ans = 0;
+        if(m<c) return -1;
+        if(m>c*8) return c-1;
+        while(m>0 && m-8 >= c-1){
+            ans++;
+            m-=8;
+            c--;
+        }
+        if(m==4 && c ==1) ans--;
+        return ans;
+        
+    }
+};
