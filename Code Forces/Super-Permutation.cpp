750e719b132bf6c783d42b8f666e3e26c9546848
Gautam Kumar Mahar <105223230+gautammahar@users.noreply.github.com>
2023-04-24 23:32:32 +0530

    Create Super-Permutation.cpp

---
+++ Code Forces/Super-Permutation.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+vector<int> generateArray(int n) {
+    vector<int> arr;
+    for (int i = 0; i < n / 2; i++) {
+        arr.push_back(i);
+        arr.push_back(n - i - 1);
+    }
+    return arr;
+}
+
+void printArray(int n, vector<int> arr) {
+    int cs = 0;
+    cout << n << " ";
+    int i = 0;
+    while (i < n - 1) {
+        cs += i;
+        cout << (arr[i + 1] - arr[i] + n) % n << " ";
+        i++;
+    }
+    cout << endl;
+}
+
+int main() {
+    int testop;
+    cin >> testop;
+
+    while (testop--) {
+        int size;
+        cin >> size;
+
+        if (size == 1) {
+            cout << "1" << endl;
+            continue;
+        }
+        if (size & 1) {
+            cout << "-1" << endl;
+            continue;
+        }
+
+        vector<int> array = generateArray(size);
+        printArray(size, array);
+    }
+
+    return 0;
+}
