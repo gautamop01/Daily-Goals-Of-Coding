0cae4438edb8790fb402fbff0d24a175d1143007
Gautam Kumar Mahar <105223230+gautammahar@users.noreply.github.com>
2023-05-02 15:14:47 +0530

    Create Sign of the Product of an Array.cpp

---
+++ Leet Code/Sign of the Product of an Array.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int arraySign(vector<int>& nums) {
+        // Check if the input vector is empty
+        if(nums.empty()) {
+            return 1;
+        }
+        
+        // Compute the product of all the elements in the input vector
+        double product = 1;
+        for(int i = 0; i < nums.size(); i++){
+            if(nums[i] == 0) {
+                return 0;
+            }
+            product *= nums[i];
+        }
+        
+        // Return the sign of the product
+        return (product > 0) ? 1 : ((product < 0) ? -1 : 0);
+    }
+};
+
+
