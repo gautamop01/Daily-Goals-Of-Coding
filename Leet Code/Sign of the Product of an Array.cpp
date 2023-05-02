abc78ca8767be54f0fda48f97d123fc279d0cb26
Gautam Kumar Mahar <105223230+gautammahar@users.noreply.github.com>
2023-05-02 15:16:12 +0530

    Update Sign of the Product of an Array.cpp

--- Leet Code/Sign of the Product of an Array.cpp
+++ Leet Code/Sign of the Product of an Array.cpp
@@ -16,7 +16,7 @@ public:
         }
         
         // Return the sign of the product
-        return (product > 0) ? 1 : ((product < 0) ? -1 : 0);
+        return (product > 0) ? 1 : ((product < 0) ? -1 : 0); // check product value is less then 0 or not 
     }
 };
 
