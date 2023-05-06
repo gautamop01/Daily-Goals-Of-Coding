2fb904d758fd3e806593e96ff7eb8ef64d848268
Gautam Kumar Mahar <105223230+gautammahar@users.noreply.github.com>
2023-05-07 03:29:26 +0530

    Update 121. Best Time to Buy and Sell Stock.cpp

--- Leet Code/2 Weeks study Plan to Tackle DS/121. Best Time to Buy and Sell Stock.cpp
+++ Leet Code/2 Weeks study Plan to Tackle DS/121. Best Time to Buy and Sell Stock.cpp
@@ -48,5 +48,41 @@ public:
 };
 
 /*
-// Correct But TLE
+// Correct But TLE Int This Code
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int n = prices.size();
+        int min = prices[0];
+        int max ;
+        int store;
+        for(int i=0;i<n;i++){
+            if(prices[i]<min){
+                min = prices[i];
+                store = i;
+            }
+            max = prices[store];
+            for(int j=store;j<n;j++){
+            if(prices[j]>max){
+                max = prices[j];
+            }
+            }
+        }
+        int answer = max - min;
+        return answer;
+    }
+};
+
+int main() {
+    Solution solution;
+    vector<int> prices = {7,6,4,3,1};
+    cout << "Max profit: " << solution.maxProfit(prices) << endl;
+    return 0;
+}
+
 */
