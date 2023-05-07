1c3c57fd24e8f6448f1e86c953c57fa542997064
Gautam Kumar Mahar <105223230+gautamop01@users.noreply.github.com>
2023-05-07 17:55:32 +0530

    Update Reshape the Matrix.cpp

--- Leet Code/2 Weeks study Plan to Tackle DS/Reshape the Matrix.cpp
+++ Leet Code/2 Weeks study Plan to Tackle DS/Reshape the Matrix.cpp
@@ -1,25 +1,9 @@
-// class Solution {
-// public:
-//     vector<vector<int>> matrixReshape(vector<vector<int>>& mat, int r, int c) {
-//         int n = mat.size();
-//   vector<vector<int>> m(n);
-
-//   for(int i=0,k=0; i<r,k<n;i++,k++){
-//       for(int j=0,l=0;j<c,l<n;j++,l++){
-//           m[i][j]=mat[k][l];
-//       }
-//   }
-//   return mat;
-  
-//     }
-// };
 
 class Solution {
 public:
     vector<vector<int>> matrixReshape(vector<vector<int>>& mat, int r, int c) {
         int n = mat.size();
         int m = mat[0].size();
-
         if(n*m != r*c){
         return mat;
         }
