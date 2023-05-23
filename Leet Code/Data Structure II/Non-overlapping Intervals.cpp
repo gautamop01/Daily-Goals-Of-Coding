f3e70fd8ead51bcb1d7ed20a63f47d003b9617b4
Gautam Kumar Mahar <105223230+gautamop01@users.noreply.github.com>
2023-05-23 19:58:31 +0530

    Update Non-overlapping Intervals.cpp

--- Leet Code/Data Structure II/Non-overlapping Intervals.cpp
+++ Leet Code/Data Structure II/Non-overlapping Intervals.cpp
@@ -1,19 +1,30 @@
 class Solution {
 public:
+    static bool compare(const vector<int>& a, const vector<int>& b) {
+        return a[1] < b[1];  // Sort intervals based on the end time
+    }
+
     int eraseOverlapIntervals(vector<vector<int>>& intervals) {
         int n = intervals.size();
-        int m = intervals[0].size();
-        int ans=0;int k=0;
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-             if(intervals[j][i]==intervals[k][i]){
-                 k++;
-                 ans++;
-             }
+        if (n == 0)
+            return 0;
+
+        // Sort intervals based on the end time
+        sort(intervals.begin(), intervals.end(), compare);
+
+        int ans = 1;  // Count of non-overlapping intervals
+        int prevEnd = intervals[0][1];  // End time of the previous interval
+
+        for (int i = 1; i < n; i++) {
+            if (intervals[i][0] >= prevEnd) {
+                // Non-overlapping interval found
+                ans++;
+                prevEnd = intervals[i][1];
             }
         }
-        return ans;
 
-        
+        // Number of intervals to be removed = Total intervals - Non-overlapping intervals
+        int removeCount = n - ans;
+        return removeCount;
     }
 };
