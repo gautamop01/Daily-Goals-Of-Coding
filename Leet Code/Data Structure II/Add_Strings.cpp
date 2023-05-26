43c92561160dea6f1f49ad453cfbcee53b8780fb
Gautam Kumar Mahar <105223230+gautamop01@users.noreply.github.com>
2023-05-26 07:35:04 +0530

    Update Add_Strings.cpp

--- Leet Code/Data Structure II/Add_Strings.cpp
+++ Leet Code/Data Structure II/Add_Strings.cpp
@@ -32,3 +32,36 @@ public:
         return result;
     }
 };
+
+/*
+// With Explanation 
+#include <string>
+
+class Solution {
+public:
+    string addStrings(string num1, string num2) {
+        int carry = 0; // Initialize a variable to hold the carry value
+        string result; // Initialize an empty string to store the result
+        int i = num1.length() - 1; // Initialize a pointer for the last digit of num1
+        int j = num2.length() - 1; // Initialize a pointer for the last digit of num2
+
+        // Iterate through the digits of both numbers and the carry value
+        while (i >= 0 || j >= 0 || carry > 0) {
+            // Get the current digits from num1 and num2 (or 0 if no more digits left)
+            int digit1 = (i >= 0) ? (num1[i--] - '0') : 0;
+            int digit2 = (j >= 0) ? (num2[j--] - '0') : 0;
+
+            // Calculate the sum of the digits and the carry
+            int sum = digit1 + digit2 + carry;
+            carry = sum / 10; // Update the carry with the tens digit of the sum
+            result = to_string(sum % 10) + result; // Append the units digit to the result
+
+            // Move to the next digit in each number
+        }
+
+        return result; // Return the final result string
+    }
+};
+
+
+*/
