2a9133f749bb69bcc269f50707dd041f025b32c9
Gautam Kumar Mahar <105223230+gautamop01@users.noreply.github.com>
2023-05-29 23:34:57 +0530

    Create Longest Palindromic Substring.cpp

---
+++ Leet Code/Data Structure II/Longest Palindromic Substring.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+class Solution { 
+public:
+    string longestPalindrome(string s) {
+        if (s.empty())
+            return "";
+
+        int start = 0;
+        int maxLength = 1;
+        int n = s.length();
+
+        // Create a 2D boolean vector to store the palindrome information
+        vector<vector<bool>> dp(n, vector<bool>(n, false));
+
+        // All substrings of length 1 are palindromes
+        for (int i = 0; i < n; i++)
+            dp[i][i] = true;
+
+        // Check for substrings of length 2
+        for (int i = 0; i < n - 1; i++) {
+            if (s[i] == s[i + 1]) {
+                dp[i][i + 1] = true;
+                start = i;
+                maxLength = 2;
+            }
+        }
+
+        // Check for substrings of length greater than 2
+        for (int len = 3; len <= n; len++) {
+            for (int i = 0; i <= n - len; i++) {
+                int j = i + len - 1;
+                if (s[i] == s[j] && dp[i + 1][j - 1]) {
+                    dp[i][j] = true;
+                    start = i;
+                    maxLength = len;
+                }
+            }
+        }
+
+        return s.substr(start, maxLength);
+    }
+};
