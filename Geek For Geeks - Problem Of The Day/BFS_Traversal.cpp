a35081ab09427450c50d2e2213f241947dcc70cf
Gautam Kumar Mahar <105223230+gautamop01@users.noreply.github.com>
2023-07-31 20:13:53 +0530

    Update BFS_Traversal.cpp

--- Geek For Geeks - Problem Of The Day/BFS_Traversal.cpp
+++ Geek For Geeks - Problem Of The Day/BFS_Traversal.cpp
@@ -2,28 +2,29 @@ class Solution {
 public:
     // Function to return Breadth First Traversal of given graph.
     vector<int> bfsOfGraph(int V, vector<int> adj[]) {
-        vector<int> bfsTraversal;
+        vector<int> bfsTraversal; // To store the BFS traversal result
         vector<bool> visited(V, false); // To keep track of visited vertices
-        queue<int> q;
+        queue<int> q; // Queue to perform BFS
 
         // Start BFS from the first vertex (vertex 0)
-        q.push(0);
-        visited[0] = true;
+        q.push(0); // Enqueue the starting vertex (vertex 0) into the queue
+        visited[0] = true; // Mark the starting vertex as visited
 
+        // Continue the BFS traversal until the queue becomes empty
         while (!q.empty()) {
-            int currentVertex = q.front();
-            q.pop();
-            bfsTraversal.push_back(currentVertex);
+            int currentVertex = q.front(); // Get the front vertex from the queue
+            q.pop(); // Dequeue the front vertex
+            bfsTraversal.push_back(currentVertex); // Add the current vertex to the BFS result
 
             // Visit all the adjacent vertices of the current vertex
             for (int neighbor : adj[currentVertex]) {
-                if (!visited[neighbor]) {
-                    visited[neighbor] = true;
-                    q.push(neighbor);
+                if (!visited[neighbor]) { // If the adjacent vertex is not visited
+                    visited[neighbor] = true; // Mark the adjacent vertex as visited
+                    q.push(neighbor); // Enqueue the adjacent vertex into the queue to explore later
                 }
             }
         }
 
-        return bfsTraversal;
+        return bfsTraversal; // Return the BFS traversal result
     }
 };
