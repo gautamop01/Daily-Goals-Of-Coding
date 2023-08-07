c4e7ae25f0e41c82f9c9dc1f05c83e8034be8929
Gautam Kumar Mahar <105223230+gautamop01@users.noreply.github.com>
2023-08-07 12:06:15 +0530

    Update Sudoku_Solver.cpp

--- Sudoku_Solver.cpp
+++ Sudoku_Solver.cpp
@@ -91,3 +91,46 @@ int main() {
 
 
 //--------------------------------------------------------LEETCODE PROBLEM---------------------------------------
+
+
+class Solution {
+public:
+    void solveSudoku(vector<vector<char>>& m) {
+        solve(m);
+        
+    }
+    
+    bool solve(vector<vector<char>> & m){
+        for(int i=0;i<m.size();i++){
+            for(int j=0;j<m[0].size();j++){
+                if(m[i][j] == '.'){
+                    for(char c='1';c<='9';c++){
+                    if(isValid(m,i,j,c)){
+                        m[i][j]=c;
+                        if(solve(m) == true)
+                            return true;
+                        else
+                            m[i][j]='.';
+                    }
+                    }
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+        
+        bool isValid(vector<vector<char>>& m, int row, int col, char c){
+            for(int i=0;i<9;i++){
+                if(m[i][col]==c)
+                    return false;
+                
+                if(m[row][i]==c)
+                    return false;
+                
+                if(m[3*(row/3) + i /3][3*(col/3) + i%3] == c)
+                    return false;
+            }
+        return true;
+    }
+};
