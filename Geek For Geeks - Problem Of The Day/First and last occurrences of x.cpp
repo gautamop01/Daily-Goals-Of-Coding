9f244eaa0263d4899c7cfc200dc3367aec089e5c
Gautam Kumar Mahar <105223230+gautamop01@users.noreply.github.com>
2023-09-22 03:33:04 +0530

    Update First and last occurrences of x.cpp

--- Geek For Geeks - Problem Of The Day/First and last occurrences of x.cpp
+++ Geek For Geeks - Problem Of The Day/First and last occurrences of x.cpp
@@ -1,3 +1,4 @@
+/*
 #include <bits/stdc++.h>
 using namespace std;
 
@@ -26,3 +27,69 @@ public:
         return ans;
     }
 };
+*/
+
+
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// bool flag = false;  // for printing only one subsequence
+// void printS(int ind, vector<int> &ds, int s, int sum, int arr[], int n){
+//     if(ind ==n){
+//         if(s == sum && flag == false){
+//             flag = true;
+//             for(auto it : ds) cout << it << " ";
+//             cout << endl;
+//         }
+//         return;
+//     }
+//     ds.push_back(arr[ind]);
+//     s += arr[ind];
+
+//     printS(ind+1,ds,s,sum,arr,n);
+
+//     s-=arr[ind];
+//     ds.pop_back();
+
+//     printS(ind+1,ds,s,sum,arr,n);
+
+    
+// }
+
+// int main(){
+//     int arr[] = {1, 2, 1};
+//     int n = 3;
+//     int sum = 2;
+//     vector<int> ds;
+//     printS(0, ds, 0, sum, arr, n);
+//     return 0;
+// }  
+
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int printS(int ind, int s, int sum, int arr[], int n){
+    if(ind == n){
+        if(s==sum) return 1;
+        else return 0;
+    }
+
+    s+=arr[ind];
+    int l = printS(ind + 1, s, sum, arr, n);
+    s-=arr[ind];
+    int r = printS(ind+1,s,sum,arr,n);
+    return l+r;
+    
+}
+
+int main(){
+    int arr[] = {1, 2, 1};
+    int n = 3;
+    int sum = 2;
+    vector<int> ds;
+   int ans = printS(0, 0, sum, arr, n);
+   cout<<ans<<endl;
+    return 0;
+}  
